8aa192ef71bfc244b42c7dfa6eb9aa5bcd7fc87b
jjdavid87 <davidchung.pepc@gmail.com>
2026-07-21 09:16:03 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "echonote-v4";
+const CACHE = "echonote-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
